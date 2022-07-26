8f45d84b9754b86a5679d75c6f6be49f6e4be5d7
thanhphuc <phuchtpc01818@fpt.edu.vn>
2022-07-26 20:16:47 +0700

    update layput user 7 page

--- DTN_ASM_JV6/src/main/java/dtn/asm/controller/user/HomeController.java
+++ DTN_ASM_JV6/src/main/java/dtn/asm/controller/user/HomeController.java
@@ -12,9 +12,37 @@ public class HomeController {
 		
 		return "/user/home/index";
 	}
-	
 	@RequestMapping("/shop.html")
 	public String shopPage() {
 		return "/user/home/shop";
 	}
+	@RequestMapping("/about.html")
+	public String about() {
+		return "/user/home/about";
+	}
+	@RequestMapping("/contact.html")
+	public String contact() {
+		return "/user/home/contact";
+	}
+	@RequestMapping("/product.html")
+	public String product() {
+		return "/user/home/product";
+	}
+	@RequestMapping("/login.html")
+	public String login() {
+		return "user/home/login";
+	}
+	@RequestMapping("/register.html")
+	public String register() {
+		return "user/home/register";
+	}
+	@RequestMapping("/changepass.html")
+	public String changepass() {
+		return "user/home/changepass";
+	}
+	@RequestMapping("/forgot-password.html")
+	public String forgot_password() {
+		return "user/home/forgot-password";
+	}
+
 }
