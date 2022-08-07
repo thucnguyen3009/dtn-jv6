dc4776a129e5b87ac22440922ecec775d21c16ee
GemLove <locbkafc123@gmail.com>
2022-08-08 00:25:04 +0700

    ok

--- DTN_ASM_JV6/src/main/resources/static/user/js/custom.js
+++ DTN_ASM_JV6/src/main/resources/static/user/js/custom.js
@@ -1,15 +1,42 @@
-// var app = angular.module("myapp",[]);
-//         app.controller("myctr", function ($scope,$location) {
-//             //Start Delete Address
-//             $scope.deleteAddress = function(adr,x){
-//                 $scope.address=adr;
-//                 $scope.id=x;
-//             };
-//             $scope.post = function() {
-//                 $location.url('/delete/'+$scope.id);
-//             }
-//             //End Delete Address
-//         })
-function deleteAddress(adr,id){
+//Start Delete Address
+function deleteAddress(adr, id) {
     $(".address").text(adr);
-    $("#deleteAddress").attr("href", "/DTNsBike/update_account.html/delete/"+id);}
+    $("#deleteAddress").attr("href", "/DTNsBike/update_account.html/delete/" + id);
+}
+//End Delete Address
+
+//Start Update Address
+function updateAddress(adr,id) {
+    $('#editAddress').val(adr);
+ $("#updateAddress").attr("formaction", "/DTNsBike/update_account.html/update/" + id);
+}
+function updateAdr() {
+    var adr = String($("#editAddress").val());
+    if (adr.trim().length == 0) {
+        $(".errorsAdr").text("Không bỏ trống ô nhập địa chỉ");
+        return false;
+    } else { 
+        return true; }
+}
+//End Update Address
+//Start Create Address
+function createAdr() {
+    var adr = String($("#newAddress").val());
+    if (adr.trim().length == 0) {
+        $("#errorsAdr").text("Không bỏ trống ô nhập địa chỉ");
+        return false;
+    } else { 
+        $("#errorsAdr").text("");
+        return true; }
+}
+
+$(document).ready(function () {
+
+    $('textarea').keypress(function (event) {
+
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
+    });
+});
+//End Create Address
